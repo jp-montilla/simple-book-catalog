f370e2c46b87aad632e1ad18bcf1413d63c93f71
John Paul Montilla <johnpaul.montilla1330@gmail.com>
2025-02-20 02:59:01 +0800

    table design

--- index.php
+++ index.php
@@ -4,8 +4,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/index.css" rel="stylesheet">
 </head>
 <body>
-    <h1>Hello World</h1>
+    <div class="container">
+        <div class="container-end">
+            <button class="btn btn-success border-0 success">Add</button>
+        </div>
+    
+    
+        <table class>
+        <tr>
+            <th>Title</th>
+            <th>ISBN</th>
+            <th>Author</th>
+            <th>Publisher</th>
+            <th>Year Published</th>
+            <th>Category</th>
+            <th class="actions"></th>
+        </tr>
+        <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+            <td>Germany</td>
+            <td>Germany</td>
+            <td>Germany</td>
+            <td class="actions">
+                <button class="btn btn-secondary border-0 secondary">Edit</button>
+                <button class="btn btn-secondary border-0 secondary">Del</button>
+            </td>
+        </tr>
+        <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+            <td>Germany</td>
+            <td>Germany</td>
+            <td>Germany</td>
+            <td class="actions">
+                <button class="btn btn-secondary border-0 secondary">Edit</button>
+                <button class="btn btn-secondary border-0 secondary">Del</button>
+            </td>
+        </tr>
+        </table>
+
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
